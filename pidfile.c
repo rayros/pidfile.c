ca5d7de293c087791c8eb975512337c877a1138c
Paweł Łaski <zniwus@gmail.com>
2015-03-19 00:46:58 +0100

    update

--- pidfile.c
+++ pidfile.c
@@ -1,14 +1,10 @@
 #include "pidfile.h"
 
 int
-lock_pid_file(int *ptr, char *file_name) {
+i_lock_pid_file(int *ptr, char *file_name) {
   *ptr = open(file_name, O_CREAT | O_RDWR, 0666); // open file
   int rc = flock(*ptr, LOCK_EX | LOCK_NB); // lock access to open file
   if(rc && EWOULDBLOCK == errno) // check lock success
     return -1; // another instance is running
   return 0; // success
-}
-
-int unlock_pid_file(int *ptr) {
-  return close(*ptr);
 }
--- pidfile.h
+++ pidfile.h
@@ -5,13 +5,14 @@
 #include <sys/file.h>
 #include <errno.h>
 
-/* return 0 if ok or -1 if another instance is running */
-int
-lock_pid_file(int *ptr,         /* int *ptr - pointer for open file */
-              char *file_name); /* char *file_name file name: (char *) "program.pid" */
 
-/* return 0 if close or -1 if fail. */
-int 
-unlock_pid_file(int *ptr); /* int *ptr - open file pointer */
+/* return 0 if ok or -1 if another instance is running */
+#define lock_pid_file(ptr, file_name) i_lock_pid_file(&ptr, file_name)
+int
+i_lock_pid_file(int *ptr,  /* int *ptr - pointer for open file */
+         char *file_name); /* char *file_name file name: (char *) "program.pid" */
+
+/* int *ptr - open file pointer */
+#define unlock_pid_file(ptr) close(ptr) 
 
 #endif
